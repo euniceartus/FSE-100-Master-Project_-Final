e83cd210509176f8e43f447639c949b90d6b27b3
emacia24 <160548416+emacia24@users.noreply.github.com>
2024-03-25 16:27:02 -0700

    Update game4.js

--- Game4/game4.js
+++ Game4/game4.js
@@ -1,10 +1,35 @@
-function game4Preload(){
-  
-}
+let maze = [
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 0, 1],
+  [1, 1, 1, 0, 1],
+  [0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1]
+];
 
-function game4Setup(){
-  background("white");
-  currentActivity = 4;
+let cellSize = 40;
+let playerSize = 20;
+let playerX;
+let playerY;
+
+function game4Preload(){
+}
+  function game4Setup() {
+    createCanvas(400, 400);
+    textAlign(CENTER, CENTER);
+    currentActivity = 4;
+    background("#fadac1");
+    let mazeWidth = maze[0].length * cellSize;
+    let mazeHeight = maze.length * cellSize;
+    let offsetX = (width - mazeWidth) / 2;
+    let offsetY = (height - mazeHeight) / 2;
+
+    playerX = offsetX + cellSize / 2;
+    playerY = offsetY + cellSize / 2;
+
+
+
+ 
+
   
   // Hide the Activity 4 button, show all the other buttons
   menuButton.show();
@@ -15,22 +40,49 @@ function game4Setup(){
 }
 
 function game4Draw(){
-  background("white");
+  background("#fadac1");
   
-  fill("white");
+  drawMaze();
+  drawPlayer();
+
+  fill("#fadac1");
   rect(10, 10, 100, 25);
   fill("black");
   text("Home Page", 25, 25);
+}
   
-  fill('black');
-  text('Activity 4 goes here', 200, 200);
+  function drawMaze() {
+
+    let mazeWidth = maze[0].length * cellSize;
+    let mazeHeight = maze.length * cellSize;
+    let offsetX = (width - mazeWidth) / 2;
+    let offsetY = (height - mazeHeight) / 2;
+
+    for (let i = 0; i < maze.length; i++) {
+        for (let j = 0; j < maze[i].length; j++) {
+            if (maze[i][j] === 1) {
+                fill(255);
+                rect(offsetX + j * cellSize, offsetY + i * cellSize, cellSize, cellSize);
+            }
+        }
+    }
 }
 
-/*****
-* Instead of using buttons like other games, this example draws 
-* rectangles and circles. The mousePressed function determines if the
-* user clicked on one of the shapes.
-*****/
-function game4MousePressed(){
-  
+function drawPlayer() {
+    fill("red");
+    ellipse(playerX, playerY, playerSize, playerSize);
 }
+
+function mouseMoved() {
+    
+    let newX = constrain(mouseX, cellSize / 2, width - cellSize / 2);
+    let newY = constrain(mouseY, cellSize / 2, height - cellSize / 2);
+
+    let cellX = floor((newX - (width - maze[0].length * cellSize) / 2) / cellSize);
+    let cellY = floor((newY - (height - maze.length * cellSize) / 2) / cellSize);
+
+    if (maze[cellY][cellX] === 0) {
+        playerX = newX;
+        playerY = newY;
+    }
+  }
