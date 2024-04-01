84b1c35e3be737fa8cb8b1ad69f8a34c4a2ffe7e
HalimaMusse <160547979+HalimaMusse@users.noreply.github.com>
2024-03-31 18:45:00 -0700

    game2

--- Game2/Game2/game2.js
+++ Game2/Game2/game2.js
@@ -19,25 +19,25 @@ function game2Draw() {
 
   // Toolbar
   fill("#e6e6e6");
-  rect(50, 10, 700, 40);
+  rect(150, 20, 700, 40);
 
   // Color palette
   fill("#50A3AB");
-  rect(100, 15, 30, 30);
+  rect(150, 25, 30, 30);
   fill("#F79D84");
-  rect(150, 15, 30, 30);
+  rect(200, 25, 30, 30);
   fill("#A8D0E6");
-  rect(200, 15, 30, 30);
+  rect(250, 25, 30, 30);
   // Add more color options as needed
 
   // Brush size selection
-  fill("black");
+  fill("#50A3AB");
   ellipse(650, 100, 20, 20); // Small brush
   ellipse(677, 100, 30, 30); // Medium brush
   ellipse(715, 100, 40, 40); // Large brush
 
   // Tool selection
-  fill("black");
+  fill("#50A3AB");
   rect(700, 150, 30, 30); // Pen tool
   rect(700, 200, 30, 30); // Eraser tool
   // Add more tools as needed
@@ -46,28 +46,31 @@ function game2Draw() {
   textSize(27);
   fill("black");
   textStyle(BOLD);
-  text("Finger Painting Game", 250, 30);
+  text("Finger Painting Game", 300, 30);
 
+  fill("#50A3AB");
+  ellipse(638, 70, 90, 35, 6);
   textSize(15);
-  fill("black");
+  fill("white");
   textStyle(BOLD);
-  text("Brush Size", 600, 70);
+  text("Brush Size", 600, 75);
 
+  fill("#50A3AB");
+  ellipse(665, 145, 50, 30, 6);
   textSize(15);
-  fill("black");
+  fill("white");
   textStyle(BOLD);
-  text("Edit", 600, 1500);
+  text("Edit", 650, 150);
 
   fill("#50A3AB");
   ellipse(75, 50, 90, 35, 6);
   textSize(15);
-  fill("Black");
+  fill("white");
   textFont(mont);
-
   text("Back",51,55);
 
   // Reset button
-  fill("#FF7F7F");
+  fill("#F6723A");
   rect(600, 10, 70, 30);
   fill("white");
   textSize(15);
