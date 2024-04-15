185b2cc3186e688e7de319664d644471403d2a8e
faghayev <160548478+faghayev@users.noreply.github.com>
2024-04-15 15:13:03 -0700

    Update game3.js

--- Game3/game3.js
+++ Game3/game3.js
@@ -6,42 +6,35 @@ function game3Preload(){
 }
 
 function game3Setup(){
-  // background(220);
-  // currentActivity = 3;
   
-  // // Hide the Activity 3 button, show all the other buttons
-  // menuButton.show();
-  // game1Button.show();
-  // game2Button.show();
-  // game3Button.hide();
-  // game4Button.show();
-  
-  // stroke(0);
-  // fill("red");
-  // circle(100,100, 200);
-  // fill("blue");
-  // rect(200,0, 200,200);
-  // fill("green");
-  // circle(300,300, 200);
-  // fill("yellow");
-  // rect(0,200, 200,200);
-  // noStroke();
 }
+ draw() {
+    image(this.img, this.pos.x, this.pos.y);
+  }
+  
+  hits(hitpos: p5.Vector) {
+    if(hitpos.x > this.pos.x && 
+       hitpos.x < this.pos.x + this.width && 
+       hitpos.y > this.pos.y && 
+       hitpos.y < this.pos.y + this.height) {
+      return true;
+    }
+    return false;
+  }
+}
+
 
 function game3Draw(){
   clear();
   background("#fadac1");
   
-  textSize(27);
-  fill("black")
-  textStyle(BOLD);
-  text("Activity 3 goes here:",250,50);
-
   fill("#50A3AB");
   ellipse(75, 50, 90, 35, 6);
+  textStyle(BOLD);
   textSize(15);
-  fill("Black");
+  fill("White");
   textFont(mont);
   text("Back",51,55);
+  
 }
 
