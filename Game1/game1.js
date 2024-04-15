47cb7b917499d9d250fe0972d2fdb1d3d234974f
euniceartus <eartus@asu.edu>
2024-04-14 22:21:41 -0700

    changed layout of piano game

--- Game1/game1.js
+++ Game1/game1.js
@@ -1,40 +1,30 @@
-let sound1;
+function Tile(x) {
+  this.w = 80;
+  this.h = 100;
+  this.x = x;
+  this.y = this.h;
+  this.moveSpeed = 5;
 
-function game1Preload(){
-  sound1 = loadSound("sound1.mp3");
+  this.show = function() {
+    strokeWeight(2);
+    fill(0, 0, 255);
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  this.move = function() {
+    this.y += this.moveSpeed;
+  }
+
+  this.clicked = function(x, y) {
+    return (x > this.x &&
+      x < this.x + this.w &&
+      y > this.y &&
+      y < this.y + this.h);
+  }
+
+  this.checkOffScreen = function() {
+    if (this.y > height) {
+      gameOver();
+    }
+  }
 }
-
-function game1Setup(){
-
-  // currentActivity = 1;
-  
-  // Hide the Game 1 button, show all the other navigation buttons
-  // menuButton.show();
-  // game1Button.hide();
-  // game2Button.show();
-  // game3Button.show();
-  // game4Button.show();
-  
-  // Set the size of the owl image
-  // owlImage.resize(150, 150);
-}
-
-function game1Draw(){
-  clear();
-  background("#fadac1");
-  
-  textSize(27);
-  fill("black")
-  textStyle(BOLD);
-  text("Activity 1 goes here:",250,50);
-
-  fill("#50A3AB");
-  ellipse(75, 50, 90, 35, 6);
-  textSize(15);
-  fill("Black");
-  textFont(mont);
-  text("Back",51,55);
-  
-}
-
-
