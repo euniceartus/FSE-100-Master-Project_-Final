a2d9af0713115ec9b33da43f644f53a0436f9fd0
faghayev <160548478+faghayev@users.noreply.github.com>
2024-04-15 15:29:05 -0700

    Update game3.js

--- Game3/game3.js
+++ Game3/game3.js
@@ -6,35 +6,15 @@ function game3Preload(){
 }
 
 function game3Setup(){
+  // background(220);
+  // currentActivity = 3;
   
 }
- draw() {
-    image(this.img, this.pos.x, this.pos.y);
-  }
-  
-  hits(hitpos: p5.Vector) {
-    if(hitpos.x > this.pos.x && 
-       hitpos.x < this.pos.x + this.width && 
-       hitpos.y > this.pos.y && 
-       hitpos.y < this.pos.y + this.height) {
-      return true;
-    }
-    return false;
-  }
-}
-
 
 function game3Draw(){
   clear();
   background("#fadac1");
   
-  fill("#50A3AB");
-  ellipse(75, 50, 90, 35, 6);
-  textStyle(BOLD);
-  textSize(15);
-  fill("White");
-  textFont(mont);
-  text("Back",51,55);
   
 }
 
