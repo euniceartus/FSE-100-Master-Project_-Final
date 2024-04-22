6dd4173c5be703791c3b4cc9c9f5d58ccb4aeb41
faghayev <160548478+faghayev@users.noreply.github.com>
2024-04-22 15:14:12 -0700

    Update game3.js
    yo

--- Game3/game3.js
+++ Game3/game3.js
@@ -2,19 +2,18 @@ let exercise3x = 200;
 let exercise3y = 200;
 
 function game3Preload(){
-  
+
 }
 
 function game3Setup(){
-  // background(220);
-  // currentActivity = 3;
+   background(220);
+  currentActivity = 3;
   
 }
 
 function game3Draw(){
   clear();
   background("#fadac1");
-  
-  
+
 }
 
