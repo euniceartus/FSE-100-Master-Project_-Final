7cba1a667f913dff1bf7af5a444ecce3b726c556
euniceartus <eartus@asu.edu>
2024-04-22 18:35:19 -0700

    Revert "Merge branch 'main' of https://github.com/euniceartus/FSE-100-Master-Project_-Final"
    This reverts commit 7e0b0ae46ab87adbbbe8a46438272c2f71a56d53, reversing
    changes made to e23688ebd30f7b774d1c76d310cae4747026d51d.

--- Game3/game3.js
+++ Game3/game3.js
@@ -1,58 +1,19 @@
-let currentWord = "";
-let userInput = "";
-let lastWordChange = 0;
-let wordChangeDelay = 2000; // 2 seconds
+let exercise3x = 200;
+let exercise3y = 200;
 
-function game3Preload() {
+function game3Preload(){
 
 }
 
-function game3Setup() {
-  createCanvas(400, 400);
-  background(220);
+function game3Setup(){
+   background(220);
   currentActivity = 3;
-  startGame();
+  
 }
 
-function game3Draw() {
+function game3Draw(){
   clear();
   background("#fadac1");
-  textAlign(CENTER);
-  textSize(24);
-  fill(0);
-  text(currentWord, width / 2, height / 2);
 
-  textSize(16);
-  text(userInput, width / 2, height / 2 + 30);
-
-  if (millis() - lastWordChange > wordChangeDelay) {
-    startGame();
-  }
 }
 
-function startGame() {
-  currentWord = getRandomWord();
-  userInput = "";
-  lastWordChange = millis();
-}
-
-function checkWord() {
-  if (userInput === currentWord) {
-    startGame();
-  }
-}
-
-function keyPressed() {
-  if (keyCode >= 65 && keyCode <= 90) { // Check if the key is a letter
-    userInput += key;
-  } else if (keyCode === BACKSPACE) {
-    userInput = userInput.slice(0, -1);
-  } else if (keyCode === ENTER) {
-    checkWord();
-  }
-}
-
-function getRandomWord() {
-  let words = ["hello", "world", "programming", "javascript", "challenge"];
-  return random(words);
-}
